7b852ed6118a0bb9928aed00b3ec0f7cad893b45
poojapadar29 <poojapadar29@gmail.com>
2023-09-22 17:49:18 -0700

    variable Assignment
    variable Assignment

---
+++ 01_variable.js
@@ -0,0 +1,31 @@
+var firstName; // variable declaration
+
+firstName = "Pooja";  // Variable initialization
+
+console.log(firstName);
+
+firstName = "Mrs.Pooja"; // Variable update
+
+console.log(firstName);
+
+ 
+
+ 
+
+var lastName = "Padar"; // variable declaration and initialization
+
+console.log(lastName);
+
+ 
+
+var height; // Variable Declaration
+
+console.log(height);
+
+height = "5.5"; // initialization
+
+console.log(height);
+
+height = "5.7 feet";// update
+
+console.log(height);
---
+++ 01_variableAssign.js
@@ -0,0 +1,29 @@
+var myFullName
+console.log(myFullName);
+myFullName = "Pooja Padar"
+console.log(myFullName);
+myFullName = "Pooja Shrirang padar"
+console.log(myFullName);
+
+var myLove = "I love only JS"
+console.log(myLove);
+
+var firstName = "Pooja"
+console.log("firstName:", firstName);
+var lastName = "padar"
+console.log( "lastName:", lastName);
+var age = "25"
+console.log("age:", age);
+var address = "Hinjewadi , Pune"
+console.log("address:", address);
+var pinCode = "411018"
+console.log("pinCode:", pinCode);
+var gender = "female"
+console.log("gender :", gender );
+var isMarried = "yes"
+console.log("isMarried :", isMarried);
+
+
+
+
+
