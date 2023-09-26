cf2c654681d5de43abf3f812be0756de4bfc7898
poojapadar29 <poojapadar29@gmail.com>
2023-09-26 16:32:05 -0700

    Assignment updated
    Assignment updated

---
+++ 03_funAssign02.js
@@ -0,0 +1,40 @@
+console.log("-------Bank Details------------");
+console.log('-------- Bank 1--------------');
+function bankDetails( bankName, accountNum, location, pinCode,){
+    var bankName = 'CITI Bank';
+    console.log("Bank Name :", bankName );
+    var accountNum = 3456782345;
+    console.log("Account Number:", accountNum);
+    var location = "Pune";
+    console.log("Location:", location);
+    var pinCode = 431202;
+    console.log("Pin Code:", pinCode);
+}
+bankDetails()
+
+
+
+
+console.log('---------Bank 2------------');
+
+var bankName = 'Axis Bank';
+console.log("Bank Name :", bankName );
+var accountNum = 7856782345;
+console.log("Account Number:", accountNum);
+var location = "Mumbai";
+console.log("Location:", location);
+var pinCode = 441202;
+console.log("Pin Code:", pinCode);
+
+
+
+console.log('---------Bank 3-------------');
+
+var bankName = 'HDFC Bank';
+console.log("Bank Name :", bankName );
+var accountNum = 8956782345;
+console.log("Account Number:", accountNum);
+var location = "Pune";
+console.log("Location:", location);
+var pinCode = 631202;
+console.log("Pin Code:", pinCode);
