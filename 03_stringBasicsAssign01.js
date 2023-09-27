ca6e032f8104469a931c4e07d371cdd66b80c0ba
poojapadar29 <poojapadar29@gmail.com>
2023-09-26 17:15:28 -0700

    Assignment
    Assignment

---
+++ 03_stringBasicsAssign01.js
@@ -0,0 +1,23 @@
+console.log('--------Step 1------------');
+function stringBasics(){
+    var myDreamCompany = "TCS"
+    console.log("My Dream Company is :", myDreamCompany);
+}
+stringBasics()
+
+console.log('-------Step 2---------');
+function myHobbies ( hobby1, hobby2, hobby3){
+    var myHobbies = " Reading , Writing, traveling"
+    console.log( "My Hobbies are :", myHobbies);
+
+}
+myHobbies()
+
+console.log("---------Step 3--------------");
+var greet =' Reading , Writing, traveling' ;
+var totalLength = greet.length;
+console.log("TOtal Number of Characters in My Hobby is: ", totalLength);
+
+
+
+
